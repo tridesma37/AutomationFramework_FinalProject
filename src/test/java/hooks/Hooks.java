9b17fda6cbf20ed141323363abde770fb06e0e35
Tridesma Essurgani <tridesma.essurgani77@gmail.com>
2026-07-08 22:13:44 +0700

    Run Selenium in headless mode for GitHub Actions

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
@@ -12,13 +13,13 @@ public class Hooks {
 
     @Before("@web")
     public void setUp() {
-
         WebDriverManager.chromedriver().setup();
-
-        driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
     }
 
     @After("@web")
